e05107fd8d16876865b0eec9daf061a81c31ab1a
Sagar Goswami <goswamisagard@gmail.com>
2023-03-07 19:21:12 -0500

    changes to folder structure

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-# git clone 
+
+
+# 1. git clone 
+
+# 2. Get Data
+
+# Replacing the raw data with a fresh copy / Replace with GCP
+rm -rf Data/01_raw
+# copying the data files / GCP
+cd ..
+robocopy TeamProphecyRawData "Team-Prophecy" /e
+cd "Team-Prophecy"
 
 # Creating a conda environment from the YAML file
 conda env create -n prophecy --file prophecy_env.yml
@@ -6,5 +17,5 @@ conda env create -n prophecy --file prophecy_env.yml
 # Activating the conda environment
 conda activate prophecy
 
-# Replacing the raw data with a fresh copy
-rm -rf Data/01_raw
+# Building the Data
+python Code/src/build_data.py
