766d2ce2b6e8b996bb99fd665e7054756bd7cf70
Sagar Goswami <goswamisagard@gmail.com>
2023-03-11 08:13:36 -0500

    Added the get_data() function

--- Code/src/modules/eda.py
+++ Code/src/modules/eda.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 
-def summarize(col:str, df:pd.DataFrame, enforce_counts:bool = False):
+def summarize_one(col:str, df:pd.DataFrame, enforce_counts:bool = False):
 
     # Check if the column is in the dataframe
     if col not in df.columns:
@@ -25,6 +25,21 @@ def summarize(col:str, df:pd.DataFrame, enforce_counts:bool = False):
     
     print(f"There are {n_missing} missing values in the {col} column.")
 
-def summarize_entire(df:pd.DataFrame, enforce_counts:bool = False):
-    for col in df.columns:
-        summarize(col, df, enforce_counts)
+def summarize(df:pd.DataFrame, cols:list = [], enforce_counts:bool = False):
+    """
+    Summarize a dataframe or a list of columns in a dataframe.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe to summarize.
+    cols : list, optional
+        The columns to summarize.
+    enforce_counts : bool, optional
+        Whether to enforce the value counts for all columns. The default is False.
+    """
+    if cols == []:
+        cols = df.columns
+    
+    for col in cols:
+        summarize(df, col, enforce_counts)
