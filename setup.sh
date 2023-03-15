ae75eadc48155040cc18e6c956819e424a672a24
Sagar Goswami <sgoswam@gmu.edu>
2023-03-15 07:24:04 -0400

    BaseModel_BETA_01

--- setup.sh
+++ setup.sh
@@ -1,55 +1,57 @@
 # This script will setup the environment for the project
-
+####################################################################################################
 # Requirements:
-# - gitgub cli
-# - GCP functions
-# - Mason OneDrive
-# - anaconda
-
-# 1. Install github cli
-
-
-# 2. Clone the private GitHub repository
-gh repo clone GoswamiSagarD/Team-Prophecy
-
-# 3. Get Data
-rm -rf Data/01_raw
-# 3.1. Get the data from the GCP bucket
-
-# 3.2. Get the data from the OneDrive folder
-cd ..
-robocopy TeamProphecyRawData "Team-Prophecy" /e
+#   - wsl for Windows only
+#   - conda
+#   - git
+####################################################################################################
+# Clone the Project Repository
+####################################################################################################
+# Using curl
+echo "################################################################################"
+echo "Cloning the repository using curl..."
+cd ".."
+curl -u "Authorization: token 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" https://github.com/GoswamiSagarD/Team-Prophecy.git
+git clone -b main https://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@github.com/GoswamiSagarD/Team-Prophecy.git
+####################################################################################################
+# Using github cli
+#
+####################################################################################################
+# Ingest the Raw Data
+####################################################################################################
+# Using OneDrive
+echo "################################################################################"
+echo "Ingesting the raw data from OneDrive..."
+cp -r "TeamProphecyRawData/Data" "Team-Prophecy"
+mkdir "Team-Prophecy/Data/02_processed" "Team-Prophecy/Data/03_final"
 cd "Team-Prophecy"
-
-# 4. Install conda
-sudo apt-get update
-sudo apt-get install wget
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-source ~/.bashrc
-# 5. Create a conda environment from the YAML file
-conda env create -n prophecy --file prophecy_env.yml
-
-# 6. Activate the conda environment
-conda activate prophecy
-
-# 7. Create the Folder Structure
-mkdir Data/02_processed
-mkdir Data/03_final
-
-# 8. Build the Dataset
-python Code/src/DataEngineering/build_data.py
-
-# 9. Create and update the log file
-# adding username, datetime, and completition text to the log file
-whoami >> log.txt
-date >> log.txt
-echo "Setup Completed" >> log.txt
-
-# 10. Print the script execution time and completion message
-echo "Setup Completed"
-echo "Time taken to execute the script:  seconds"
-
-# 11. Wait for 10 seconds and Exit the script
-sleep 10
+####################################################################################################
+# Using GCP
+#
+####################################################################################################
+# Create and Update the Conda Environment
+####################################################################################################
+#   Check if environment exists
+echo "################################################################################"
+echo "Setting up the Conda environment..."
+if conda info --envs | grep -q "prophecy"; then
+  # Update environment from YAML file
+  conda env update --file "prophecy_env.yml"
+else
+  # Create environment from YAML file
+  conda env create -f "prophecy_env.yml"
+fi
+####################################################################################################
+# Activate the Conda Environment
+source activate prophecy
+####################################################################################################
+# Build the Dataset
+####################################################################################################
+echo "################################################################################"
+echo "Building the dataset..."
+python build_data.py
+echo "Done! The project is go for Data Science! Late Night Coding didn't kill me, but it did make this happen."
+echo "- Sagar Goswami"
+sleep 30
 exit
+####################################################################################################
